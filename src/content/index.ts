3072ccda0c94ceeaf1bb571a9881ff7c67355bb3
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-08-29 02:28:01 +0200

    feat: add form data extraction

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,4 +1,7 @@
 document.addEventListener('submit', function (event) {
     const form = event.target;
     if (!(form instanceof HTMLFormElement)) return;
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
 }, true);
